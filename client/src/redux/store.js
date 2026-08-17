21c1bdaa962856d2afa5a46a97533ad67a3c2f06
Mikel <ugochukwumikel15@gmail.com>
2026-08-17 17:45:20 +0100

    add redux process

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+
+// Custom storage engine for Vite compatibility
+const createWebStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(localStorage.getItem(_key));
+    },
+    setItem(_key, value) {
+      return Promise.resolve(localStorage.setItem(_key, value));
+    },
+    removeItem(_key) {
+      return Promise.resolve(localStorage.removeItem(_key));
+    },
+  };
+};
+
+const storage = typeof window !== 'undefined' ? createWebStorage() : null;
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
-  })
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
